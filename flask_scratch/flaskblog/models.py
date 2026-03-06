8616dd7c9de9f8121e5c6305f3f60f5edecb3444
Mingliboyev <jahongirmengliboyev575@gmail.com>
2026-03-06 10:00:45 +0500

    Full Flask Project without server

---
+++ flask_scratch/flaskblog/models.py
@@ -0,0 +1,42 @@
+from flaskblog import db, app
+from datetime import datetime
+from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f'User({self.username}, {self.email}, {self.image_file})'
+    
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id':self.id}, salt='password-reset-salt')
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, salt='password-reset-salt', max_age=1800)['user_id']
+        except:
+            return None
+        return User.query.get(user_id) 
+    
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
